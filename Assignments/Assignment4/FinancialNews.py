c48b2bf913980fed6c92f4d2de47414df494e687
MilanMangukiya <milanmangukiya22@gmail.com>
2025-06-13 20:44:38 -0400

    assignment

--- Assignments/Assignment4/FinancialNews.py
+++ Assignments/Assignment4/FinancialNews.py
@@ -11,25 +11,11 @@ from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.messages import HumanMessage, AIMessage
 from langgraph.graph import StateGraph,END
-from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from dotenv import load_dotenv
-import re
-
-def clean_text_list(docs):
-    cleaned_list = []
-    for doc in docs:
-        # Extract text content from Document object
-        text = doc.page_content  # or doc.content depending on your Document class
-        # Now clean the raw text string
-        text = text.replace('\\t', ' ')
-        text = text.replace('\\n', ' ')
-        text = re.sub(r'\s+', ' ', text)
-        text = text.strip()
-        cleaned_list.append(text)
-    return cleaned_list
 
 load_dotenv()
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
@@ -39,19 +25,28 @@ model = ChatGroq(model="deepseek-r1-distill-llama-70b")
 embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en")
 
 # Loading the documents
-loader=DirectoryLoader("C:\Projects-Agentic\Agentic AI\Agentic-AI\Assignments\Assignment4",glob="*.pdf",loader_cls=PyPDFLoader)
+loader = PyMuPDFLoader(r"Assignments/Assignment4/tsla_Q1.pdf")
 docs = loader.load()
-print(f"Loaded {len(docs)} documents")
-# print(docs)
-print(type(docs))
-
-clean_text = clean_text_list(docs)
-for item in clean_text:
-    print(item) 
 
 # split the documents into smaller chunks
 text_splitter=RecursiveCharacterTextSplitter(
-    chunk_size=200,
-    chunk_overlap=50
+    chunk_size=300,
+    chunk_overlap=75
 )
+
+chunks = text_splitter.split_documents(documents=docs)
+
+# Create a vector store from the chunks
+vector_store = Chroma.from_documents(
+    documents=chunks
+    ,embedding=embeddings
+    ,persist_directory="Assignments/Assignment4/financial_news_vector_store"
+)
+
+retriever = vector_store.as_retriever(
+        search_type="mmr",
+        search_kwargs={'k': 3, 'fetch_k': 30}
+)
+
+
 
